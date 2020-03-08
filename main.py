987e00046f4a11b10033c4cc87e43a1b29694d25
MQZON <bmo@pdx.edu>
2020-03-07 19:14:50 -0800

    Added projectiles

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ class Player:
     rotation = 0
     velocity = 1
     lives = 3
+    IMAGE = "player.png"
 
     def __init__(self, x, y, rotation):
         self.x = x
         self.y = y
         self.rotation = rotation
         velocity = 1
+
+class Projectile:
+    x = 100
+    y = 100
+    rotation = 0
+    velocity = 5
+
+    def __init__(self, x, y, rotation):
+        self.x = x
+        self.y = y
+        self.rotation = rotation
+        velocity = 5
 
 class Asteroid:
     x = 50
@@ -39,8 +52,10 @@ def main():
     pygame.display.set_caption("Asteroids Genetic Algorithm")
 
     player = Player(100, 100, 0)
-    ship = pygame.image.load("player.png")
+    ship = pygame.image.load(player.IMAGE)
     ship = pygame.transform.rotate(ship, -90)
+
+    projectiles = []
 
     timer =  pygame.time.Clock()
 
@@ -59,8 +74,18 @@ def main():
             if player.velocity <= 50: player.velocity += 5
         if keys[pygame.K_DOWN]:
             if player.velocity >= 6: player.velocity -= 5
+        if keys[pygame.K_SPACE]:
+            projectiles.append(fireProjectile(player, ship))
 
-        #calculate new position.
+        win.fill(BLACK)
+
+        #calculate and draw projectiles
+        for each in projectiles:
+            each.x += math.cos(math.radians(each.rotation))*each.velocity
+            each.y -= math.sin(math.radians(each.rotation))*each.velocity
+            pygame.draw.line(win, (255, 255, 255), (each.x, each.y), (each.x, each.y))
+
+        #calculate new ship position.
         angle = math.radians(player.rotation)
         xcomp = math.cos(angle)
         ycomp = math.sin(angle)
@@ -73,13 +98,19 @@ def main():
         if player.y < 0: player.y += WINDOW_HEIGHT
         if player.x > WINDOW_WIDTH: player.x -= WINDOW_WIDTH
         if player.x < 0: player.x += WINDOW_WIDTH
-        win.fill(BLACK)
         newship = rotatedShip.get_rect(center = ship.get_rect(topleft = (player.x, player.y)).center)
         win.blit(rotatedShip, newship.topleft)
+
         pygame.display.update()
         timer.tick(60)
 
     pygame.quit()
+
+def fireProjectile(player, ship):
+    x = player.x + ship.get_rect().centerx
+    y = player. y + ship.get_rect().centery
+    fire = Projectile(x, y, player.rotation)
+    return fire
 
 if __name__ == '__main__':
     main()
